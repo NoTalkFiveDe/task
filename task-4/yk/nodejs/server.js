c091d771f69f60ab191f8e980990f08ded091f40
Jax <jaxvanyang@gmail.com>
2021-03-24 13:49:09 +0800

    实现有 bug 的多人终端聊天室

--- task-4/yk/nodejs/server.js
+++ task-4/yk/nodejs/server.js
@@ -1,23 +1,32 @@
 const net = require('net');
 
+const clientList = [];
+
 const server = net.createServer((c) => {
     // 'connection' 监听器。
     console.log('客户端已连接');
-    c.on('end', () => {
-        console.log('客户端已断开连接');
-    });
+    clientList.push(c); // 记录客户端
+    console.log('已添加客户端: ' + c.address() + '\r\n');
+
     c.write('你好\r\n');
     // c.pipe(c);
     c.on('data', (data) => {
         console.log('client: ' + data.toString());
-        c.write('message from client: ' + data.toString());
+        // c.write('message from client: ' + data.toString());
+        broadcast(data);
         // c.write('recieved\r\n');
     });
-    c.on('end', () => {
 
+    c.on('end', () => {
+        console.log('客户端已断开连接');
     });
 });
 
+function broadcast(data) {
+    for (var i = 0; i < clientList.length; ++i) {
+        clientList[i].write(data);
+    }
+}
 
 // 异常处理
 server.on('error', (err) => {
