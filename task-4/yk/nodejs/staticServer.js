71add9936996000bbfa5f4d74bcd92b4a9da6c10
Jax <jaxvanyang@gmail.com>
2021-03-27 11:29:59 +0800

    简化静态服务器代码

--- task-4/yk/nodejs/staticServer.js
+++ task-4/yk/nodejs/staticServer.js
@@ -4,62 +4,55 @@ const path = require('path');
 const mime = require('mime');
 
 const cache = {};
-const port = 1024;  // 网页服务器所用端口
+const port = 8080;  // 网页服务器所用端口
 
-// 找不到页面时返回 404.md，但经过测试浏览器默认不支持 md 文件
+// 找不到页面时返回 404.md，但经过测试浏览器默认不支持渲染 md 文件
 const path404 = './public/404.md';
-const mimeTypeMd = mime.getType(path.basename(path404));
 
-fs.readFile(path404, (err, data) => {
-    if (err) {
-        cache[path404] = '404.md not found!';
-    } else {
-        cache[path404] = data;
-    }
-});
+function sendFile(res, filePath) {
+    res.writeHead(200, { 'Content-Type': mime.getType(path.basename(filePath)) });
+    res.end(cache[filePath]);
+}
 
 function send404(res) {
-    res.writeHead(404, { 'Content-Type': mimeTypeMd });
-    res.write(cache[path404]);
-    res.end();
-}
-
-// @param: 回复，文件路径，文件内容
-function sendFile(res, filePath, fileContent) {
-    res.writeHead(200, { 'content-type': mime.getType(path.basename(filePath)) });
-    res.end(fileContent);
-}
-
-function staticServer(res, absPath) {
-    if (cache[absPath]) { sendFile(res, absPath, cache[absPath]); }
-    else {
-        fs.exists(absPath, (exists) => {
-            if (exists) {
-                fs.readFile(absPath, (err, data) => {
-                    if (err) {
-                        send404(res);
-                    } else {
-                        cache[absPath] = data;
-                        sendFile(res, absPath, data);
-                    }
-                })
+    res.writeHead(404, { 'Content-Type': mime.getType(path.basename(path404)) });
+    if (!cache[path404]) {
+        fs.readFile(path404, (err, data) => {
+            if (err) {
+                console.log('Error when read 404 page');
+                cache[path404] = 'Error when read 404 page';
             } else {
-                send404(res);
+                cache[path404] = data;
             }
-        })
+        });
     }
+    sendFile(res, path404);
 }
 
 const server = http.createServer((req, res) => {
-    var filePath = null;
-
     console.log('requset url : ' + req.url);
+    var filePath = req.url;
+    if (filePath == '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + filePath;
+    }
+    filePath = './' + filePath;
 
-    if (req.url == '/') filePath = 'public/index.html';
-    else filePath = 'public' + req.url;
-
-    const absPath = './' + filePath;
-    staticServer(res, absPath);
+    if (cache[filePath]) {
+        sendFile(res, filePath);
+    } else {
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                console.log(filePath + ' not found');
+                send404(res);
+                // res.end();
+            } else {
+                cache[filePath] = data;
+                sendFile(res, filePath);
+            }
+        });
+    }
 });
 
 server.listen(port, () => {
